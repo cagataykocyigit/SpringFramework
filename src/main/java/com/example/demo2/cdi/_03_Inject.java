c648230db717b7ac18385fc4986443e6fc3e4ae6
Cagatay Kocyigit <cagatay.kocyigit@hotmail.com>
2022-08-09 00:17:45 +0300

    @Inject

---
+++ src/main/java/com/example/demo2/cdi/_03_Inject.java
@@ -0,0 +1,19 @@
+package com.example.demo2.cdi;
+
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.util.List;
+
+@Named(value = "injecttuto")
+@ApplicationScoped
+public class _03_Inject {
+
+    @Inject
+    List<String> tuketen;
+
+    public List<String> getTuketen() {
+        return tuketen;
+    }
+}
